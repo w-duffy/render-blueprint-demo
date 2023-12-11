d14a38aef5af56bb6bdfb1e707e5fec99ee02655
will <w.duffy@outlook.com>
2023-12-10 19:29:45 -0600

    trying db url

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,15 +1,31 @@
 #!/bin/sh
 
 
-echo "Waiting for PostgreSQL to start..."
-while ! nc -z db 5432; do
+# echo "Waiting for PostgreSQL to start..."
+# while ! nc -z db 5432; do
+#   sleep 0.1
+# done
+# echo "PostgreSQL started"
+
+# flask db upgrade
+
+# flask seed all
+
+# exec gunicorn app:app --bind 0.0.0.0:8000
+# exec gunicorn app:app
+
+
+
+# Extract host and port from DATABASE_URL
+db_host=$(echo $DATABASE_URL | cut -d '@' -f 2 | cut -d ':' -f 1)
+db_port=$(echo $DATABASE_URL | cut -d '@' -f 2 | cut -d ':' -f 2 | cut -d '/' -f 1)
+
+echo "Waiting for PostgreSQL to start at $db_host:$db_port..."
+while ! nc -z $db_host $db_port; do
   sleep 0.1
 done
 echo "PostgreSQL started"
 
 flask db upgrade
-
 flask seed all
-
 exec gunicorn app:app --bind 0.0.0.0:8000
-# exec gunicorn app:app
