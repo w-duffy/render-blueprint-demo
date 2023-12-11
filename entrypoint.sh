d32669683143590cd599619cc2b6237d5a53e030
will <w.duffy@outlook.com>
2023-12-11 02:58:13 -0600

    DEBUG PROD: entrypoint commit wasn't added

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,25 +1,25 @@
 #!/bin/sh
 
 # ~~~~~~~~~~~~~ This Works For Local Testing ~~~~~~~~~~~~~~~~
-echo "Waiting for PostgreSQL to start..."
-while ! nc -z db 5432; do
-  sleep 0.1
-done
-echo "PostgreSQL started"
+# echo "Waiting for PostgreSQL to start..."
+# while ! nc -z db 5432; do
+#   sleep 0.1
+# done
+# echo "PostgreSQL started"
 
-flask db upgrade
+# flask db upgrade
 
-flask seed all
+# flask seed all
 
-exec gunicorn app:app --bind 0.0.0.0:8000
+# exec gunicorn app:app --bind 0.0.0.0:8000
 # exec gunicorn app:app
 
 
 # Above works locally.  Below is for testing deploying render blueprint
 
 # ~~~~~~~~~~~~~~~~ this works for PRODUCITON using a blueprint defined in the render.yaml:~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-# echo "PostgreSQL started"
+echo "PostgreSQL started"
 
-# flask db upgrade
-# flask seed all
-# exec gunicorn app:app
+flask db upgrade
+flask seed all
+exec gunicorn app:app
