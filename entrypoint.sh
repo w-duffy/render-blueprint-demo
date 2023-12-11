3720fb9f8d62fcf029791fbdc3f2e643a6c11a13
will <w.duffy@outlook.com>
2023-12-10 19:37:08 -0600

    trying db url

--- entrypoint.sh
+++ entrypoint.sh
@@ -15,15 +15,15 @@
 # exec gunicorn app:app
 
 
-
+# Above works locally.  Below is for testing deploying render blueprint
 # Extract host and port from DATABASE_URL
-db_host=$(echo $DATABASE_URL | cut -d '@' -f 2 | cut -d ':' -f 1)
-db_port=$(echo $DATABASE_URL | cut -d '@' -f 2 | cut -d ':' -f 2 | cut -d '/' -f 1)
+# db_host=$(echo $DATABASE_URL | cut -d '@' -f 2 | cut -d ':' -f 1)
+# db_port=$(echo $DATABASE_URL | cut -d '@' -f 2 | cut -d ':' -f 2 | cut -d '/' -f 1)
 
-echo "Waiting for PostgreSQL to start at $db_host:$db_port..."
-while ! nc -z $db_host $db_port; do
-  sleep 0.1
-done
+# echo "Waiting for PostgreSQL to start at $db_host:$db_port..."
+# while ! nc -z $db_host $db_port; do
+#   sleep 0.1
+# done
 echo "PostgreSQL started"
 
 flask db upgrade
